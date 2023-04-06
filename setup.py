d5ec2efc702c86c1c7d20a3d9194e4663eed4797
JarbasAi <jarbasai@mailfence.com>
2023-04-06 23:41:06 +0100

    feat/automations

--- setup.py
+++ setup.py
@@ -1,11 +1,41 @@
+import os
 from setuptools import setup
+
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
+
+def get_version():
+    """ Find the version of the package"""
+    version = None
+    version_file = os.path.join(BASEDIR, 'precise_lite_runner', 'version.py')
+    major, minor, build, alpha = (None, None, None, None)
+    with open(version_file) as f:
+        for line in f:
+            if 'VERSION_MAJOR' in line:
+                major = line.split('=')[1].strip()
+            elif 'VERSION_MINOR' in line:
+                minor = line.split('=')[1].strip()
+            elif 'VERSION_BUILD' in line:
+                build = line.split('=')[1].strip()
+            elif 'VERSION_ALPHA' in line:
+                alpha = line.split('=')[1].strip()
+
+            if ((major and minor and build and alpha) or
+                    '# END_VERSION_BLOCK' in line):
+                break
+    version = f"{major}.{minor}.{build}"
+    if alpha and int(alpha) > 0:
+        version += f"a{alpha}"
+    return version
+
 
 setup(
     name='precise_lite_runner',
-    version='0.4.0',
+    version=get_version(),
     packages=['precise_lite_runner'],
     url='https://github.com/OpenVoiceOS/precise_lite_runner',
     license='Apache-2.0',
+    include_package_data=True,
     install_requires=["sonopy==0.1.2",
                       "pyaudio"],
     extras_require={
